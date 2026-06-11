8a4611b18f208c9c1569068358ad3b4d5008ad10
Rifa Mayshakori <rifamaysha45@gmail.com>
2026-06-11 16:47:57 +0700

    initialize recommendation module with imports

---
+++ recommendation/__init__.py
@@ -0,0 +1,4 @@
+from .anomaly import detect_anomalies
+from .budget import analyze_budget, BUCKET_DISPLAY, IDEAL_RATIO
+
+__all__ = ["detect_anomalies", "analyze_budget", "BUCKET_DISPLAY", "IDEAL_RATIO"]
